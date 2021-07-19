9da33459b1965dbae9bbe647e5b5eb11c3cef12e
shivaprasad <57038598+shivaprasad-kulal@users.noreply.github.com>
2021-07-19 10:40:53 +0530

    Add files via upload

--- include/header.php
+++ include/header.php
@@ -1,1 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="css/header.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">The Sparks Foundation</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
 
+                    <li class="nav-item">
+                        <a class="nav-link" href="user.php">Money Transfer</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="transfer_h.php">Transfer History</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
+<style type="text/css">
+    .nav-link:hover {
+        background:#d7b3ff;
+        color: white;
+        border-radius: 8px;
+    }
+    .navbar-brand{
+        background:#d7b3ff;
+        border-radius: 8px;
+    }
+    .navabr-brand:hover {
+        background: white;
+    }
+	
+</style>
